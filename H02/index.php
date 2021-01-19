99b9a6e4dd571912c9191d663ba49c30b35eb568
NeLLo <officialnellorecords@gmail.com>
2021-01-19 17:09:15 +0100

    H2 afgemaakt

---
+++ H02/index.php
@@ -0,0 +1,33 @@
+<?php
+    $a = "Ik heb niet gefaald";
+    $b = "die niet werken";
+    $c = 10000;
+
+    echo $a.". Ik heb alleen ".$c." manieren gevonden ".$b.".";
+
+    echo "<br>";
+    echo "<br>";
+
+    $d = "die geen fout";
+    $e = "en";
+    $f = "niet.";
+
+    echo "Mensen ".$d.$e." mak".$e.", werk".$e." ".$f;
+
+    echo "<br>";
+    echo "<br>";
+
+    $g = "ry";
+    $h = ", but";
+    $i = "fail";
+
+    echo "T".$g." and ".$i.$h." never ".$i." to t".$g;
+
+    echo "<br>";
+    echo "<br>";
+
+    $j = "\"omgaan";
+    $k = "met";
+    $l = "teleu";
+
+    echo "De cursus ".$j." ".$k." ".$l."rstellingen"."\" kan vanavond helaas niet doorgaan.";
